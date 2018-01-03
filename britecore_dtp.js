e2e423a468079e4ffb71ea54806d05c834f0151c
Erica Lee <erica.mr.lee@gmail.com>
2018-01-02 22:21:14 -0600

    Clean up HTML, CSS, and JavaScript

--- britecore_dtp.js
+++ britecore_dtp.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
-    // console.log('loaded')
-    // $("#draggable").draggable({
-    //     cancel: false,
-    //     helper: "clone"
-    // })
-
     $( "#sort-form" ).sortable({
-        cancel: false,
         cursor: "move"
-    });
-    $( "#sort-form" ).disableSelection({
-        cancel: false,
     });
 })
